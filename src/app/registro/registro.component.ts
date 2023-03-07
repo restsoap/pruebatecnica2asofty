2c2f2e51123777f5a236d824030c6100810d35d2
Brayan Cañon <brayan13canon@gmail.com>
2023-03-07 12:26:00 -0500

    registro de cambio

---
+++ src/app/registro/registro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.scss']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor( 
+    
+  ) { }
+
+  ngOnInit(): void {
+      
+    }
+  }
+
+
